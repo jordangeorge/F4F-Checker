6025d4bac4eee2cded84e971a4ee74eddf9c470b
Jordan George <jordanngeorge@gmail.com>
2019-10-16 21:41:30 -0500

    edit script and readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Follow for Follow (F4F) Instagram Checker
 The main use case is checking if average people you followed through a 'follow for follow' agreement are following you back.
+
+##### Utilization Details
+- View Chrome version for ChromeDriver by selecting "Chrome" in the menu bar. Then select "About Google Chrome".
+- Download ChromeDriver [here](https://chromedriver.chromium.org/downloads)
+- Move to downloaded repo
+- Put Facebook credentials into a config.py file
+- Run with `python checker.py`
--- checker.py
+++ checker.py
@@ -48,22 +48,26 @@ class InstagramChecker():
         time.sleep(2)
         page = self.driver.page_source
         soup = bs4.BeautifulSoup(page, 'lxml')
-        ul_div = soup.find_all('ul')[2]
+        ul_div = soup.find_all('ul')
+        if len(ul_div) == 3:
+            ul_div = ul_div[2]
+        elif len(ul_div) == 4:
+            ul_div = ul_div[3]
 
         following_usernames = list()
-        for ul in ul_div:
-            lis = ul.find_all('li')
-            for li in lis:
-                try: status = li.find_all('div')[0].find_all('div')[2].find_all('div')[0].find('span').contents[0]
-                except: status = 'not'
 
-                # if user is verified, don't add to username list
-                if status == 'Verified':
-                    continue
-                # if user is not verified, add to username list
-                else:
-                    username = li.find_all('div')[0].find_all('div')[2].find_all('div')[0].find('a').contents[0]
-                    following_usernames.append(username)
+        lis = ul_div.find_all('li')
+        for li in lis:
+            try: status = li.find_all('div')[0].find_all('div')[0].find_all('div')[1].find('span').contents[0]
+            except: status = 'not'
+
+            # if user is verified, don't add to username list
+            if status == 'Verified':
+                continue
+            # if user is not verified, add to username list
+            else:
+                username = li.find_all('div')[0].find_all('div')[0].find_all('div')[1].find('a').contents[0]
+                following_usernames.append(username)
 
         return following_usernames
 
@@ -88,14 +92,18 @@ class InstagramChecker():
         time.sleep(2)
         page = self.driver.page_source
         soup = bs4.BeautifulSoup(page, 'lxml')
-        ul_div = soup.find_all('ul')[2]
+        ul_div = soup.find_all('ul')
+        if len(ul_div) == 3:
+            ul_div = ul_div[2]
+        elif len(ul_div) == 4:
+            ul_div = ul_div[3]
 
         followers_usernames = list()
-        for ul in ul_div:
-            lis = ul.find_all('li')
-            for li in lis:
-                username = li.find_all('div')[0].find_all('div')[2].find_all('div')[0].find('a').contents[0]
-                followers_usernames.append(username)
+
+        lis = ul_div.find_all('li')
+        for li in lis:
+            username = li.find_all('div')[0].find_all('div')[0].find_all('div')[1].find('a').contents[0]
+            followers_usernames.append(username)
 
         return followers_usernames
 

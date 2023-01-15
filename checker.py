5025c9bee86dfc5859593c4bc5ed1e57cd5aa441
Jordan George <jordanngeorge@gmail.com>
2023-01-15 13:38:37 -0600

    Add quotation mark consistency

--- checker.py
+++ checker.py
@@ -15,8 +15,8 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 
 logging.basicConfig(
-    filename='status.log',
-    format='%(asctime)s - %(levelname)s - %(message)s',
+    filename="status.log",
+    format="%(asctime)s - %(levelname)s - %(message)s",
     level=logging.INFO
 )
 
@@ -24,10 +24,10 @@ load_dotenv()
 
 class InstagramChecker():
     def __init__(self):
-        self.driver = webdriver.Chrome('./chromedriver')
-        self.url = 'https://www.instagram.com'
+        self.driver = webdriver.Chrome("./chromedriver")
+        self.url = "https://www.instagram.com"
         self.wait = WebDriverWait(self.driver, 15)
-        self.target_profile_username = os.getenv('INSTAGRAM_USERNAME')
+        self.target_profile_username = os.getenv("INSTAGRAM_USERNAME")
 
     def login(self):
         # go to instagram
@@ -35,8 +35,8 @@ class InstagramChecker():
 
         # input username and password
         time.sleep(3)
-        self.driver.find_element_by_name('username').send_keys(self.target_profile_username)
-        self.driver.find_element_by_name('password').send_keys(os.getenv('INSTAGRAM_PASSWORD'))
+        self.driver.find_element_by_name("username").send_keys(self.target_profile_username)
+        self.driver.find_element_by_name("password").send_keys(os.getenv("INSTAGRAM_PASSWORD"))
         
         # click login button
         self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "div[class='_ab8w  _ab94 _ab99 _ab9f _ab9m _ab9p _abcm']"))).click()
@@ -70,83 +70,84 @@ class InstagramChecker():
 
         li_num = 0
         while li_num < num:
-            self.driver.execute_script('return arguments[0].scrollIntoView(0, document.documentElement.scrollHeight-10);', dialog_ul_div)
-            li_num = len(dialog_ul_div.find_elements_by_tag_name('button'))
-            # print(li_num,'<',num)
+            self.driver.execute_script("return arguments[0].scrollIntoView(0, document.documentElement.scrollHeight-10);", dialog_ul_div)
+            li_num = len(dialog_ul_div.find_elements_by_tag_name("button"))
+            # print(li_num,"<",num)
 
     def get_following(self):
-        print('Getting people ' + self.target_profile_username + ' is following')
+        print("Getting people " + self.target_profile_username + " is following")
 
         # get number of following
         time.sleep(4)
         num_of_following = int(self.wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "span[class='_ac2a']")))[2].text)
 
+        # todo
         # click on following dialog
         time.sleep(4)
         self.wait.until(EC.element_to_be_clickable((By.XPATH, "//a[contains(@href,'/following')]"))).click()
         
-        self.scroll_through_dialog('/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/div[1]', num_of_following)
+        self.scroll_through_dialog("/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/div[1]", num_of_following)
 
         following_usernames = list()
 
         for i in range(0, num_of_following):
-            username_and_verify_status = self.driver.find_element_by_xpath('/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/div/div/div['+str(i+1)+']/div[2]/div[1]/div/div/div/a/span/div').text.split('\n')
+            username_and_verify_status = self.driver.find_element_by_xpath("/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/div/div/div["+str(i+1)+"]/div[2]/div[1]/div/div/div/a/span/div").text.split("\n")
 
             username = username_and_verify_status[0]
             
             try:
                 verify_status = username_and_verify_status[1]
             except:
-                verify_status = '-'
+                verify_status = "-"
 
             try:
-                display_name = self.driver.find_element_by_xpath('/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/div/div/div['+str(i+1)+']/div[2]/div[2]/div').text
+                display_name = self.driver.find_element_by_xpath("/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/div/div/div["+str(i+1)+"]/div[2]/div[2]/div").text
             except:
                 display_name = "-"
 
             following_usernames.append({
-                'username': username,
-                'profile_link': self.url + "/" + username,
-                'display_name': display_name,
-                'verify_status': verify_status
+                "username": username,
+                "profile_link": self.url + "/" + username,
+                "display_name": display_name,
+                "verify_status": verify_status
             })
 
         return following_usernames
 
     def get_followers(self):
-        print('Getting people that are following ' + self.target_profile_username)
+        print("Getting people that are following " + self.target_profile_username)
 
         # get number of followers
         num_of_followers = int(self.driver.find_elements_by_css_selector("span[class='_ac2a']")[1].text)
 
         # click on followers dialog
         time.sleep(1)
-        self.driver.find_element_by_xpath('/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div[2]/section/main/div/header/section/ul/li[2]/a').click()
+        self.driver.find_element_by_xpath("/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div[2]/section/main/div/header/section/ul/li[2]/a").click()
 
-        self.scroll_through_dialog('/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]/div[1]', num_of_followers)
+        self.scroll_through_dialog("/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]/div[1]", num_of_followers)
 
         followers_usernames = list()
 
         for i in range(0, num_of_followers):
-            username_and_verify_status = self.driver.find_element_by_xpath('/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]/div[1]/div/div['+str(i+1)+']/div[2]/div[1]/div/div/div/a/span/div').text.split("\n")
+            username_and_verify_status = self.driver.find_element_by_xpath("/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]/div[1]/div/div["+str(i+1)+"]/div[2]/div[1]/div/div/div/a/span/div").text.split("\n")
 
             username = username_and_verify_status[0]
 
             try:
                 verify_status = username_and_verify_status[1]
             except:
-                verify_status = '-'
+                verify_status = "-"
 
             try:
-                display_name = self.driver.find_element_by_xpath('/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]/div[1]/div/div['+str(i+1)+']/div[2]/div[2]/div').text
+                display_name = self.driver.find_element_by_xpath("/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]/div[1]/div/div["+str(i+1)+"]/div[2]/div[2]/div").text
             except:
                 display_name = "-"
 
             followers_usernames.append({
-                'username': username,
-                'profile_link': self.url + "/" + username,
-                'display_name': display_name,
-                'verify_status': verify_status
+                "username": username,
+                "profile_link": self.url + "/" + username,
+                "display_name": display_name,
+                "verify_status": verify_status
             })
 
 
@@ -154,16 +155,16 @@ class InstagramChecker():
 
     def get_comparisons(self):
         following_usernames = self.get_following()
-        self.driver.find_element_by_xpath('/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[1]/div/div[3]/div/button').click() # close dialog window
+        self.driver.find_element_by_xpath("/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[1]/div/div[3]/div/button").click() # close dialog window
         followers_usernames = self.get_followers()
 
         result_list = list()
 
         for i in range(len(following_usernames)):
             for j in range(len(followers_usernames)):
-                if following_usernames[i]['username'] == followers_usernames[j]['username']:
+                if following_usernames[i]["username"] == followers_usernames[j]["username"]:
                     break
-                elif following_usernames[i]['username'] != followers_usernames[j]['username'] and j == len(followers_usernames)-1:
+                elif following_usernames[i]["username"] != followers_usernames[j]["username"] and j == len(followers_usernames)-1:
                     result_list.append(following_usernames[i])
 
         return result_list
@@ -173,10 +174,10 @@ class InstagramChecker():
         print("Creating ratio sorted csv file...")
 
         for user in result_list:
-            print(user['username'])
+            print(user["username"])
 
             # go to profile
-            self.driver.get(self.url + "/" + user['username'])
+            self.driver.get(self.url + "/" + user["username"])
             
             print("Getting followers")
 
@@ -189,13 +190,13 @@ class InstagramChecker():
             if "mil" in followers_string:
                 # is , being registered as . at all?
                 just_numbers = followers_string.split(" ")[0]
-                replace_comma = just_numbers.replace(',', '.')
+                replace_comma = just_numbers.replace(",", ".")
                 convert_to_float = float(replace_comma)
                 multiply = convert_to_float * 1000
                 followers_int = int(multiply)
             elif "M" in followers_string:
                 just_numbers = followers_string.strip("M")
-                replace_comma = just_numbers.replace(',', '.')
+                replace_comma = just_numbers.replace(",", ".")
                 convert_to_float = float(replace_comma)
                 multiply = convert_to_float * 1000000
                 followers_int = int(multiply)
@@ -207,7 +208,7 @@ class InstagramChecker():
             # print(followers_int)
 
             # add to dict
-            user['followers'] = followers_int
+            user["followers"] = followers_int
 
             print("Getting following")
 
@@ -220,13 +221,13 @@ class InstagramChecker():
             if "mil" in following_string:
                 # is , being registered as . at all?
                 just_numbers = following_string.split(" ")[0]
-                replace_comma = just_numbers.replace(',', '.')
+                replace_comma = just_numbers.replace(",", ".")
                 convert_to_float = float(replace_comma)
                 multiply = convert_to_float * 1000
                 following_int = int(multiply)
             elif "M" in following_string:
                 just_numbers = following_string.strip("M")
-                replace_comma = just_numbers.replace(',', '.')
+                replace_comma = just_numbers.replace(",", ".")
                 convert_to_float = float(replace_comma)
                 multiply = convert_to_float * 1000000
                 following_int = int(multiply)
@@ -238,19 +239,19 @@ class InstagramChecker():
             # print(following_int)
 
             # add to dict
-            user['following'] = following_int
+            user["following"] = following_int
 
-            user['ratio'] = round(followers_int / following_int, 1)
+            user["ratio"] = round(followers_int / following_int, 1)
 
             print(f"follower:following ratio for \"{user['username']}\": {user['ratio']}")
-            print('------------------------------')
+            print("------------------------------")
 
         # sort by ratio
         sorted_data = sorted(result_list, key=itemgetter("ratio"), reverse=True)
         # print(sorted_data)
 
         # create csv file
-        dir_name = 'csv'
+        dir_name = "csv"
         if not os.path.isdir(dir_name):
             os.mkdir("./results/" + dir_name)
 
@@ -259,14 +260,14 @@ class InstagramChecker():
         
         df = pd.DataFrame(sorted_data)
         print()
-        print(df['ratio'].head())
+        print(df["ratio"].head())
         print()
-        df.to_csv(file_path, sep=',', index=False)
+        df.to_csv(file_path, sep=",", index=False)
 
         # create pickle for df
         if not os.path.isdir(dir_name):
             os.mkdir(dir_name)
-        df.to_pickle(f'data_{current_time}.pkl')
+        df.to_pickle(f"data_{current_time}.pkl")
 
     def close_driver(self):
         self.driver.close()
@@ -274,8 +275,8 @@ class InstagramChecker():
     def __exit__(self, exc_type, exc_value, traceback):
         self.close_driver()
 
-def put_results_in_file(l, fmt_amts_str):
-    dir_name = 'text'
+def put_results_in_file(result_list, fmt_amts_str):
+    dir_name = "text"
     if not os.path.isdir(dir_name):
         os.mkdir("./results/" + dir_name)
 
@@ -284,25 +285,25 @@ def put_results_in_file(l, fmt_amts_str):
 
     results_file = open(file_path, "a")
 
-    results_file.write(str(fmt_amts_str.format('Username', 'Profile Link', 'Display Name', 'Verify Status')))
-    results_file.write(str('\n'))
-    results_file.write(str('-' * sum(fmt_amts)))
-    results_file.write(str('\n'))
+    results_file.write(str(fmt_amts_str.format("Username", "Profile Link", "Display Name", "Verify Status")))
+    results_file.write(str("\n"))
+    results_file.write(str("-" * sum(fmt_amts)))
+    results_file.write(str("\n"))
 
-    for item in l:
-        results_file.write(str(fmt_amts_str.format(item['username'], item['profile_link'], item['display_name'], item['verify_status'])))
-        results_file.write(str('\n'))
+    for item in result_list:
+        results_file.write(str(fmt_amts_str.format(item["username"], item["profile_link"], item["display_name"], item["verify_status"])))
+        results_file.write(str("\n"))
 
     results_file.close()
 
-    print(f'Results are located here: {file_path}')
+    print(f"Results are located here: {file_path}")
     print()
 
 def print_results_to_console(l, fmt_amts_str):
-    print(fmt_amts_str.format('Username', 'Profile Link', 'Display Name', 'Verify Status'))
-    print('-' * sum(fmt_amts))
+    print(fmt_amts_str.format("Username", "Profile Link", "Display Name", "Verify Status"))
+    print("-" * sum(fmt_amts))
     for item in l:
-        print(fmt_amts_str.format(item['username'], item['profile_link'], item['display_name'], item['verify_status']))
+        print(fmt_amts_str.format(item["username"], item["profile_link"], item["display_name"], item["verify_status"]))
     print()
 
 # for sorting by another column besides "ratio"
@@ -331,19 +332,19 @@ def usePickle(sort_by_column):
     print()
 
     # create csv file
-    dir_name = 'csv'
+    dir_name = "csv"
     if not os.path.isdir(dir_name):
         os.mkdir("./results/" + dir_name)
 
     current_time = datetime.now().strftime("%Y-%m-%d_%H%M%S")
     csv_file_path = "./results/" + dir_name + "/results_" + current_time + ".csv"
 
-    df.to_csv(csv_file_path, sep=',', index=False)
+    df.to_csv(csv_file_path, sep=",", index=False)
 
 if __name__ == "__main__":
-    logging.info('Started')
+    logging.info("Started")
 
-    dir_name = 'results'
+    dir_name = "results"
     if not os.path.isdir(dir_name):
         os.mkdir(dir_name)
 
@@ -365,9 +366,9 @@ if __name__ == "__main__":
 
         ic.create_ratio_sorted_csv(result_list)
 
-        fmt_amts = [25, 55, 35, len('Verify Status')]
-        fmt_amts_str = ''
-        for i in fmt_amts: fmt_amts_str += '{:' + str(i) + '}'
+        fmt_amts = [25, 55, 35, len("Verify Status")]
+        fmt_amts_str = ""
+        for i in fmt_amts: fmt_amts_str += "{:" + str(i) + "}"
 
         put_results_in_file(result_list, fmt_amts_str)
         # print_results_to_console(result_list, fmt_amts_str)
